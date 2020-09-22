a51cd75cbc4bf76f73a981222a53e09b6bde899c
tech-arichan <arihiro2940@gmail.com>
2020-09-22 12:27:18 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController < ApplicationController
+  
+  def index
+    @post = Post.all
+  end
+  
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+
+end
